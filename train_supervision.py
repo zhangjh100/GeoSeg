a2aab9c5de5cc6f2f92ee271581ba61c801cf0c2
zhangjh100 <zhangjh100@nenu.edu.cn>
2025-04-09 12:28:16 +0800

    update 39

--- train_supervision.py
+++ train_supervision.py
@@ -89,6 +89,9 @@ class Supervision_Train(pl.LightningModule):
         elif 'mmwhs' in self.config.log_name:
             mIoU = np.nanmean(self.metrics_train.Intersection_over_Union()[:-1])
             F1 = np.nanmean(self.metrics_train.F1()[:-1])
+        elif 'cell' in self.config.log_name:
+            mIoU = np.nanmean(self.metrics_train.Intersection_over_Union()[:-1])
+            F1 = np.nanmean(self.metrics_train.F1()[:-1])
         else:
             mIoU = np.nanmean(self.metrics_train.Intersection_over_Union())
             F1 = np.nanmean(self.metrics_train.F1())
@@ -146,6 +149,11 @@ class Supervision_Train(pl.LightningModule):
             Recall = np.nanmean(self.metrics_val.Recall()[:-1])
             JAC = np.nanmean(self.metrics_val.JAC()[:-1])
         elif 'mmwhs' in self.config.log_name:
+            mIoU = np.nanmean(self.metrics_val.Intersection_over_Union()[:-1])
+            F1 = np.nanmean(self.metrics_val.F1()[:-1])
+            Recall = np.nanmean(self.metrics_val.Recall()[:-1])
+            JAC = np.nanmean(self.metrics_val.JAC()[:-1])
+        elif 'cell' in self.config.log_name:
             mIoU = np.nanmean(self.metrics_val.Intersection_over_Union()[:-1])
             F1 = np.nanmean(self.metrics_val.F1()[:-1])
             Recall = np.nanmean(self.metrics_val.Recall()[:-1])
