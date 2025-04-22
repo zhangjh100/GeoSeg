e58facb2a0e75df7f89d2e3ab7ecce523c32c840
zhangjh100 <zhangjh100@nenu.edu.cn>
2025-04-22 08:59:22 +0800

    update 35

--- geoseg/datasets/synapse_dataset.py
+++ geoseg/datasets/synapse_dataset.py
@@ -12,7 +12,7 @@ from PIL import Image
 import random
 from .transform import *
 
-CLASSES = ('label_0', 'label_1', 'label_2', 'label_3', 'label_4', 'label_5', 'label_6', 'label_7')
+CLASSES = ('background', 'stomach', 'aorta', 'gallbladder', 'left kidney', 'right kidney', 'liver', 'pancreas')
 PALETTE = [[0, 0, 0], [255, 255, 255], [0, 0, 255], [0, 255, 0], [255, 0, 0], [0, 255, 255], [255, 0, 255], [255, 255, 0]]
 
 ORIGIN_IMG_SIZE = (512, 512)
@@ -54,7 +54,7 @@ def val_aug(img, mask):
 
 
 class synapseDataset(Dataset):
-    def __init__(self, data_root='data/Synapse/train', mode='train', img_dir='images', mask_dir='masks',
+    def __init__(self, data_root='data/Synapse/test', mode='test', img_dir='images', mask_dir='masks',
                  img_suffix='.png', mask_suffix='.png', transform=val_aug, mosaic_ratio=0.0,
                  img_size=ORIGIN_IMG_SIZE):
         self.data_root = data_root
