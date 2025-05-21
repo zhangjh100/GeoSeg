d7b1f9e98253d1383d945475e909919c31ab07f2
zhangjh100 <zhangjh100@nenu.edu.cn>
2025-05-21 09:53:03 +0800

    update 36

--- config/la/ftunetformer.py
+++ config/la/ftunetformer.py
@@ -42,11 +42,11 @@ use_aux_loss = False
 
 # define the dataloader
 
-train_dataset = laDataset(data_root='data/la_2018/train', mode='train',
+train_dataset = laDataset(data_root='data/la_2018/test', mode='test',
                                  mosaic_ratio=0.25, transform=train_aug)
 
 val_dataset = laDataset(transform=val_aug)
-test_dataset = laDataset(data_root='data/la_2018/train',
+test_dataset = laDataset(data_root='data/la_2018/test',
                                 transform=val_aug)
 
 train_loader = DataLoader(dataset=train_dataset,
