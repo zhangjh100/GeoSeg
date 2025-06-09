f7fea2a0fd20e5ea2faf7a2eb501d2c73c668803
zhangjh100 <zhangjh100@nenu.edu.cn>
2025-06-09 21:16:29 +0800

    update 36

--- config/acdc/resglnet.py
+++ config/acdc/resglnet.py
@@ -75,7 +75,7 @@ val_loader = DataLoader(dataset=val_dataset,
 
 # define the optimizer
 layerwise_params = {"backbone.*": dict(lr=backbone_lr, weight_decay=backbone_weight_decay)}
-net_params = process_model_params(net, layerwise_params=layerwise_params)
+net_params = process_model_params(model, layerwise_params=layerwise_params)
 base_optimizer = torch.optim.AdamW(net_params, lr=lr, weight_decay=weight_decay)
 optimizer = Lookahead(base_optimizer)
 lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingWarmRestarts(optimizer, T_0=15, T_mult=2)
