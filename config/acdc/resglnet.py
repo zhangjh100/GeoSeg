9dca28f50e98994460d4764ea4f008371cdd1d5a
zhangjh100 <zhangjh100@nenu.edu.cn>
2025-06-09 21:17:35 +0800

    update 36

--- config/acdc/resglnet.py
+++ config/acdc/resglnet.py
@@ -34,14 +34,14 @@ resume_ckpt_path = None  # whether continue training with the checkpoint, defaul
 #  define the network
 # net = UNetFormer(num_classes=num_classes)
 
-model = UNetFormer(
+net = UNetFormer(
     backbone_name='resnet18',
     pretrained=False,  # 不使用timm自动加载的预训练权重
     num_classes=6
 )
 
 # 加载自定义预训练权重
-model.load_backbone_weights(
+net.load_backbone_weights(
     weight_path='./pretrain_weights/rest_lite.pth',  # 自定义路径
     strict=False  # 允许部分加载（如缺少分类头权重）
 )
@@ -75,7 +75,7 @@ val_loader = DataLoader(dataset=val_dataset,
 
 # define the optimizer
 layerwise_params = {"backbone.*": dict(lr=backbone_lr, weight_decay=backbone_weight_decay)}
-net_params = process_model_params(model, layerwise_params=layerwise_params)
+net_params = process_model_params(net, layerwise_params=layerwise_params)
 base_optimizer = torch.optim.AdamW(net_params, lr=lr, weight_decay=weight_decay)
 optimizer = Lookahead(base_optimizer)
 lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingWarmRestarts(optimizer, T_0=15, T_mult=2)
