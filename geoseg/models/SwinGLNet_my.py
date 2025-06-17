699beebf605b06f9ab84a4bb652baa6ec137ffbd
zhangjh100 <zhangjh100@nenu.edu.cn>
2025-06-17 10:52:14 +0800

    update 36

--- geoseg/models/SwinGLNet_my.py
+++ geoseg/models/SwinGLNet_my.py
@@ -823,13 +823,30 @@ class Block(nn.Module):
         return x
 
 
+# class WF(nn.Module):
+#     def __init__(self, in_channels=128, decode_channels=128, eps=1e-8):
+#         super(WF, self).__init__()
+#         self.pre_conv = ConvBN(in_channels, decode_channels, kernel_size=3)
+#
+#         # 添加 SCSE 模块用于处理残差连接
+#         self.scse = SCSEModule(decode_channels)
+#
+#         self.weights = nn.Parameter(torch.ones(2, dtype=torch.float32), requires_grad=True)
+#         self.eps = eps
+#         self.post_conv = ConvBNReLU(decode_channels, decode_channels, kernel_size=3)
+#
+#     def forward(self, x, res):
+#         x = F.interpolate(x, scale_factor=2, mode='bilinear', align_corners=False)
+#         res = self.scse(self.pre_conv(res))
+#         weights = nn.ReLU()(self.weights)
+#         fuse_weights = weights / (torch.sum(weights, dim=0) + self.eps)
+#         x = fuse_weights[0] * self.pre_conv(res) + fuse_weights[1] * x
+#         x = self.post_conv(x)
+#         return x
 class WF(nn.Module):
     def __init__(self, in_channels=128, decode_channels=128, eps=1e-8):
         super(WF, self).__init__()
         self.pre_conv = ConvBN(in_channels, decode_channels, kernel_size=3)
-
-        # 添加 SCSE 模块用于处理残差连接
-        self.scse = SCSEModule(decode_channels)
 
         self.weights = nn.Parameter(torch.ones(2, dtype=torch.float32), requires_grad=True)
         self.eps = eps
@@ -837,7 +854,6 @@ class WF(nn.Module):
 
     def forward(self, x, res):
         x = F.interpolate(x, scale_factor=2, mode='bilinear', align_corners=False)
-        res = self.scse(self.pre_conv(res))
         weights = nn.ReLU()(self.weights)
         fuse_weights = weights / (torch.sum(weights, dim=0) + self.eps)
         x = fuse_weights[0] * self.pre_conv(res) + fuse_weights[1] * x
