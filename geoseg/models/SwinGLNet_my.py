4e933622e0d0316ff72e8f11613eef39d73b7f2d
zhangjh100 <zhangjh100@nenu.edu.cn>
2025-06-18 13:49:02 +0800

    update 36

--- geoseg/models/SwinGLNet_my.py
+++ geoseg/models/SwinGLNet_my.py
@@ -869,15 +869,10 @@ class WF(nn.Module):
 class SCSEModule(nn.Module):
     def __init__(self, ch, re=16):
         super().__init__()
-        # self.cSE = nn.Sequential(nn.AdaptiveAvgPool2d(1),
-        #                          nn.Conv2d(ch,ch//re,1),
-        #                          nn.ReLU(inplace=True),
-        #                          nn.Conv2d(ch//re,ch,1),
-        #                          nn.Sigmoid())
-        self.cSE = nn.Sequential(nn.AdaptiveMaxPool2d(1),
-                                 nn.Conv2d(ch, ch // re, 1),
+        self.cSE = nn.Sequential(nn.AdaptiveAvgPool2d(1),
+                                 nn.Conv2d(ch,ch//re,1),
                                  nn.ReLU(inplace=True),
-                                 nn.Conv2d(ch // re, ch, 1),
+                                 nn.Conv2d(ch//re,ch,1),
                                  nn.Sigmoid())
         self.sSE = nn.Sequential(nn.Conv2d(ch,ch,1),
                                  nn.Sigmoid())
